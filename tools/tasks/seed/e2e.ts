15d40a8755a0782f25323855f99da9bbda54bb25
den <2oppin@gmail.com>
2017-03-26 20:59:12 +0300

    merge angular4-seed -part2

--- tools/tasks/seed/e2e.ts
+++ tools/tasks/seed/e2e.ts
@@ -6,23 +6,19 @@ import { protractor } from 'gulp-protractor';
 import Config from '../../config';
 
 class Protractor {
-  /*
   server(port: number, dir: string) {
     let app = express();
     let root = resolve(process.cwd(), dir);
-    for (let proxy of Config.getProxyMiddleware()) {
+    for (let proxy of Config.PROXY_MIDDLEWARE) {
       app.use(proxy);
     }
     app.use(express.static(root));
     app.use(history('index.html', { root }));
     return new Promise((resolve, reject) => {
       let server = app.listen(port, () => {
-          resolve(server);
-        });
+        resolve(server);
+      });
     });
-  */
-  server(port: number) {
-    return require('../../../dist/server/prod').init(port, 'prod');
   }
 }
 
@@ -32,8 +28,7 @@ class Protractor {
 export = (done: any) => {
   process.env.LANG = 'en_US.UTF-8';
   new Protractor()
-//    .server(Config.PORT, Config.PROD_DEST)
-    .server(9005)
+    .server(Config.PORT, Config.PROD_DEST)
     .then((server: any) => {
       gulp
         .src(join(Config.DEV_DEST, '**/*.e2e-spec.js'))
